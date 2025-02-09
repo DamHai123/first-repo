34218fb4c5ce2c62908ad8ce8c75e311ce3eddf5
DamHai123 <damhaich2@gmail.com>
2025-02-09 11:19:19 +0700

    Mô tả thay đổi

--- ok.dart
+++ ok.dart
@@ -1,3 +1,8 @@
 void main() {
-  print('');
+  var hehe = [1, 2, 3, 4, 5];
+  List<String> hoho = ['1', '2', '3', '4', '5'];
+  print(hehe[2]);
+  hehe.addAll([2, 3, 4]);
+  print(hoho[2]);
+  print(hehe);
 }
