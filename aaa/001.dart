75406d4816599769e6b2a9f91de2803e65cbe3e0
DamHai123 <damhaich2@gmail.com>
2025-02-16 19:39:35 +0700

    11

--- aaa/001.dart
+++ aaa/001.dart
@@ -6,6 +6,7 @@ List<String> DS = ['Nguyen Thanh Nam', 'Tran Thi Phuong'];
 String RAM = '';
 main() {
   while (true) {
+    DS.remove('');
     print(
         '1. Nhap vao sinh vien \n 2. In ra danh sach \n 3. In ra danh sach dc sap xep \n 0.EXIT');
     String? input = stdin.readLineSync() ?? '';
@@ -19,17 +20,23 @@ main() {
           RAM = ten;
           DS.add(RAM);
           RAM = '';
+          String? input = stdin.readLineSync() ?? '';
+
         case '2':
           for (var i in DS) {
             print(i);
           }
+          String? input = stdin.readLineSync() ?? '';
+
         case '3':
           RAMM = DS;
           RAMM.sort();
           print(RAMM);
           RAMM = [];
+          String? input = stdin.readLineSync() ?? '';
+
         default:
-          print('loi, nhap lai');
+          print('loi, nhap lai \n');
       }
     }
   }
